81328ad6d63d611f3b090f59a1891ec17b4abf17
Shadow <mdurihin@gmail.com>
2024-09-19 21:37:31 +0300

    done

---
+++ index.js
@@ -0,0 +1,54 @@
+// const num = 20; 
+
+// const result = num > 10 && num < 30; 
+
+
+// console.log(result);
+
+
+// const num = 5; 
+
+// const result = num < 10 || num > 30; 
+
+// console.log(result);
+
+
+// console.log(!true);
+
+
+// let cost = 0;
+// const subscription = 'free';
+
+// if (subscription === 'pro') {
+//   cost = 100;
+// }else{ 
+//     cost = 0; 
+// }
+
+// console.log(cost);
+
+
+// let cost = 0;
+
+// const subscription = prompt("Enter type of your sub").trim(); 
+
+// if (subscription === "pro") {
+//   cost = "100 $$$";
+// } else {
+//   cost = 0;
+// }
+
+// alert(cost); 
+
+
+let userAge = prompt("Enter your age >>> "); 
+
+if(userAge >= 16 && userAge <= 60){ 
+    alert("You are in");
+}else{ 
+    alert("Blocked for you!"); 
+    window.close(); 
+}
+
+
+
